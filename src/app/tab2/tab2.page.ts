920487f3d6a5e6687a7e3932008b84a98d406369
dudinha2004 <eduarda.araujo@souunisuam.com.br>
2024-04-25 17:48:48 -0300

    edição do app

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,8 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class CalculadoraImcPage {
+  altura: number | undefined;
+  peso: number | undefined;
+  resultado: number | undefined;
 
-  constructor() {}
+  calcularIMC() {
+    if (this.altura && this.peso) {
+      this.resultado = this.peso / ((this.altura / 100) * (this.altura / 100));
+    }
+  }
 
+  interpretarIMC(imc: number): string {
+    if (imc < 18.5) {
+      return 'Magreza';
+    } else if (imc < 24.9) {
+      return 'Normal';
+    } else if (imc < 29.9) {
+      return 'Sobrepeso';
+    } else if (imc < 39.9) {
+      return 'Obesidade';
+    } else {
+      return 'Obesidade Grave';
+    }
+  }
 }
